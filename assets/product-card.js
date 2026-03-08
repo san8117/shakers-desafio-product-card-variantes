674347b7dc59c03fc346684e1b23ec1ef304aeba
Guilherme Oliveira <san835117@gmail.com>
2026-03-08 13:27:21 -0300

    feat:searching for the IDs of the size and color options and manipulating this HTML element using e.target.

--- assets/product-card.js
+++ assets/product-card.js
@@ -1,3 +1,26 @@
+let sizeProduct, colorProduct;
+const buttonAddCart = document.querySelector('[data-btn-add-card]')
 const dataJson = document.querySelector('div[data-json]');
 const json = dataJson.getAttribute('data-json');
 console.log(JSON.parse(json));
+
+const optionSelected = document.querySelectorAll('select');
+
+optionSelected.forEach(optionSelected => {
+    console.log(optionSelected)
+})
+
+
+function changeOption() {
+    optionSelected.addEventListener('change', (e) => {
+        console.log(optionSelected.id)
+    })
+    
+     if (optionSelected == "tamanho") {
+        sizeProduct = e.target.value;
+     }
+     if (optionSelected == "cor") {
+         colorProduct = e.target.value;
+     }
+}
+
