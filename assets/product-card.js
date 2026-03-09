885cb9b2268aa75551b73002c025336725b34555
Guilherme Oliveira <san835117@gmail.com>
2026-03-08 22:01:53 -0300

    style: page styling

--- assets/product-card.js
+++ assets/product-card.js
@@ -1,26 +1,63 @@
 let sizeProduct, colorProduct;
-const buttonAddCart = document.querySelector('[data-btn-add-card]')
-const dataJson = document.querySelector('div[data-json]');
-const json = dataJson.getAttribute('data-json');
-console.log(JSON.parse(json));
+const buttonAddCart = document.querySelector('[data-btn-add-cart]')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const dataJson = document.querySelector('div[data-json]');
+    const json = dataJson.getAttribute('data-json');
+    console.log(JSON.parse(json));
+    let variantId = json.id;
+    console.log(variantId)
+
+})
 
 const optionSelected = document.querySelectorAll('select');
 
 optionSelected.forEach(optionSelected => {
-    console.log(optionSelected)
-})
 
-
-function changeOption() {
     optionSelected.addEventListener('change', (e) => {
         console.log(optionSelected.id)
     })
-    
-     if (optionSelected == "tamanho") {
+
+    if (optionSelected == "tamanho") {
         sizeProduct = e.target.value;
-     }
-     if (optionSelected == "cor") {
-         colorProduct = e.target.value;
-     }
+    }
+    if (optionSelected == "cor") {
+        colorProduct = e.target.value;
+    }
+
+    console.log(colorProduct, sizeProduct);
+
+    json.forEach(json => {
+        if (json.option1 == colorProduct && json.option2 == sizeProduct) {
+            console.log(json)
+        }
+    });
+})
+
+
+function addCart() {
+    let formData = {
+        'items': [{
+            'id': 7114244980785,
+            'quantity': 1
+        }]
+    };
+
+    fetch(window.Shopify.routes.root + 'cart/add.js', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+        buttonAddCart.addEventListener('click', addCart);
 }
+
 
